689a1688287ad4fbaee6653def06b21fda9fa6dc
Guillermo Rauch <rauchg@gmail.com>
2016-07-01 14:31:31 -0700

    install: only prod deps

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ export npm_config_runtime=electron
 # Tell node-pre-gyp to build module from source code.
 export npm_config_build_from_source=true
 # Install all dependencies, and store cache to ~/.electron-gyp.
-HOME=~/.electron-gyp npm install
+HOME=~/.electron-gyp npm install --only=prod
