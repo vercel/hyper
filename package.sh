af482e0eb4a5d2b6ca78b5a3782302d15e01bb36
Guillermo Rauch <rauchg@gmail.com>
2016-07-01 16:20:02 -0700

    dist -> build

--- package.sh
+++ package.sh
@@ -4,6 +4,7 @@ rm -rf node_modules
 rm -rf ./app/node_modules
 rm -rf ./dist/HyperTerm-darwin-x64
 rm -rf ./dist/
+rm -rf ./build/
 mkdir dist
 ./install.sh
 npm run lint
@@ -12,6 +13,7 @@ npm install
 npm run lint
 npm run build
 cd -
-cp app/index.html dist/
-cp -r app/dist dist/
+cp app/index.html build/
+cp -r app/dist build/
 electron-packager ./ --platform=darwin --out=dist --arch=x64 --app-bundle-id="co.zeit.hyperterm" --app-version="$VERSION" --osx-sign --icon=icon.icns --prune --ignore=app
+rm -rf ./build/
