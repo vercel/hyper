277e4ee1d90ad9102384d695c0250ccd79cd0fdf
Abhinav I <abhinav.qd@gmail.com>
2016-07-17 07:00:12 +0530

    Fixed an issue where the app icon was not showing up in Linux - report in Issue#126 (#168)

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.on('ready', () => {
       title: 'HyperTerm',
       backgroundColor: toHex(cfg.backgroundColor || '#000'),
       transparent: true,
+      icon: resolve(__dirname, 'static/icon.png'),
       // we only want to show when the prompt
       // is ready for user input
       show: process.env.HYPERTERM_DEBUG || isDev
