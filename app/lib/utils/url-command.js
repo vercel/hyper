9d2bf6fd2173ce6efef9babbc6a1007d201a2878
Jonathan Cuthbert <jon@cuth.net>
2016-07-19 23:38:41 -0500

    allow opening of files in webview

--- app/lib/utils/url-command.js
+++ app/lib/utils/url-command.js
@@ -4,47 +4,23 @@ export const domainRegex = /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)
 
 export default function isUrlCommand (shell, data) {
   const matcher = regex[shell];
-  if (undefined === matcher) return null;
-  let url, i;
+  if (undefined === matcher || !data) return null;
 
-  switch (matcher) {
-    case regex.bash:
-      i = 5;
-      break;
+  const match = data.match(matcher);
+  if (!match) return null;
+  const protocol = match[1];
+  const path = match[2];
 
-    case regex.zsh:
-      i = 7;
-      break;
-
-    case regex.fish:
-      i = 4;
-      break;
-  }
-
-  let match = data.match(matcher);
-
-  if (match) {
-    url = match[i];
-    if (url) {
-      // extract the domain portion from the url
-      const domain = url.split('/')[0];
-      if (domainRegex.test(domain)) {
-        return toURL(url);
-      }
+  if (path) {
+    if (protocol) {
+      return `${protocol}${path}`;
+    }
+    // extract the domain portion from the url
+    const domain = path.split('/')[0];
+    if (domainRegex.test(domain)) {
+      return `http://${path}`;
     }
   }
 
   return null;
-}
-
-function toURL (domain) {
-  if (/^https?:\/\//.test(domain)) {
-    return domain;
-  }
-
-  if ('//' === domain.substr(0, 2)) {
-    return domain;
-  }
-
-  return 'http://' + domain;
 }
