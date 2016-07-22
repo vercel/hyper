1defcc94a0d84bd168dc049c3daec57d571e2a60
Lorenzo Zottar <lordgiotto@gmail.com>
2016-07-22 09:48:18 +0200

    Small fixed and added some comments

--- app/lib/utils/file.js
+++ app/lib/utils/file.js
@@ -1,10 +1,10 @@
 // Taken from https://github.com/kevva/executable and modified
 export function isExecutable (fileStat) {
-	if (process.platform === 'win32') {
-		return true;
-	}
+  if (process.platform === 'win32') {
+    return true;
+  }
 
-	return Boolean((fileStat['mode'] & parseInt('0001', 8)) ||
-		(fileStat['mode'] & parseInt('0010', 8)) && process.getgid && gid === process.getgid() ||
-		(fileStat['mode'] & parseInt('0100', 8)) && process.getuid && uid === process.getuid());
+  return Boolean((fileStat['mode'] & parseInt('0001', 8)) ||
+  (fileStat['mode'] & parseInt('0010', 8)) && process.getgid && gid === process.getgid() ||
+  (fileStat['mode'] & parseInt('0100', 8)) && process.getuid && uid === process.getuid());
 };
