e838d7cdae70217165a348a95e6f2ace51e87ae0
Lorenzo Zottar <lordgiotto@gmail.com>
2016-07-23 02:54:58 +0200

    Fixed wrong code and added detailed comment

--- app/lib/utils/file.js
+++ app/lib/utils/file.js
@@ -1,10 +1,17 @@
-// Taken from https://github.com/kevva/executable and modified
-export function isExecutable (fileStat) {
-  if (process.platform === 'win32') {
-    return true;
-  }
+// Based on https://github.com/kevva/executable
+// Since this module doesn't expose the function to check stat mode only,
+// his logic is pasted here.
+// Opened an issue and a pull request about it,
+// to maybe switch to module in the future:
+// Issue: https://github.com/kevva/executable/issues/9
+// PR: https://github.com/kevva/executable/pull/10
 
-  return Boolean((fileStat['mode'] & parseInt('0001', 8)) ||
-  (fileStat['mode'] & parseInt('0010', 8)) && process.getgid && gid === process.getgid() ||
-  (fileStat['mode'] & parseInt('0100', 8)) && process.getuid && uid === process.getuid());
+export function isExecutable (fileStat) {
+  if (process.platform === 'win32') return true;
+
+  return Boolean(
+    (fileStat['mode'] & parseInt('0001', 8)) ||
+    (fileStat['mode'] & parseInt('0010', 8)) ||
+    (fileStat['mode'] & parseInt('0100', 8))
+  );
 };
