5764b31e265a9ab81b7659b536d9ea8020472654
Jacob Wunder <jacob@jacobwunder.com>
2016-07-26 13:47:18 -0400

    Server now extends EventEmitter instead of creating and EventEmitter instance (#406)

--- app/rpc.js
+++ app/rpc.js
@@ -2,12 +2,12 @@ const { EventEmitter } = require('events');
 const { ipcMain } = require('electron');
 const uuid = require('uuid');
 
-class Server {
+class Server extends EventEmitter {
 
   constructor (win) {
+    super();
     this.win = win;
     this.ipcListener = this.ipcListener.bind(this);
-    this.emitter = new EventEmitter();
 
     if (this.destroyed) return;
 
@@ -29,27 +29,11 @@ class Server {
   }
 
   ipcListener (event, { ev, data }) {
-    this.emitter.emit(ev, data);
+    super.emit(ev, data);
   }
 
   emit (ch, data) {
     this.wc.send(this.id, { ch, data });
-  }
-
-  on (ev, fn) {
-    this.emitter.on(ev, fn);
-  }
-
-  once (ev, fn) {
-    this.emitter.once(ev, fn);
-  }
-
-  removeListener (ev, fn) {
-    this.emitter.removeListener(ev, fn);
-  }
-
-  removeAllListeners () {
-    this.emitter.removeAllListeners();
   }
 
   destroy () {
