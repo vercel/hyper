91f4c222429d3c3de948075cd9662d15d4d3f926
ayhankuru <ayhankuru@yandex.com.tr>
2016-07-27 06:04:05 +0300

    fix control

--- app/index.js
+++ app/index.js
@@ -208,11 +208,13 @@ app.on('ready', () => {
     // the window can be closed by the browser process itself
     win.on('close', () => {
       windowSet.delete(win);
-      win.close();
       rpc.destroy();
       deleteSessions();
       cfgUnsubscribe();
       pluginsUnsubscribe();
+      if(windowSet.size === 0) {
+        win.close();
+      }
     });
   }
 
