8dc96da80c9e6aea75ff2110fd9cce1b8e412dca
Matheus Fernandes <github@matheus.top>
2016-08-03 16:22:00 -0300

    Simplify multiple assertions into one

--- test/app/plugins.js
+++ test/app/plugins.js
@@ -9,13 +9,14 @@ describe('plugins', function () {
                        '@org1/project4#1.0.0', '@org2/project5@alpha',
                        '@org3/project6'];
 
-      const result = _toDependencies({plugins: plugins});
-      result.should.have.property('project1', 'latest');
-      result.should.have.property('project2', '1.0.0');
-      result.should.have.property('project3', 'beta');
-      result.should.have.property('@org1/project4', '1.0.0');
-      result.should.have.property('@org2/project5', 'alpha');
-      result.should.have.property('@org3/project6', 'latest');
+      _toDependencies({plugins: plugins}).should.be.eql({
+        'project1': 'latest',
+        'project2': '1.0.0',
+        'project3': 'beta',
+        '@org1/project4': '1.0.0',
+        '@org2/project5': 'alpha',
+        '@org3/project6': 'latest'
+      });
     });
   });
 });
