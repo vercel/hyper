fd351a5b9322d40b1924e49b493fb8031f0758ce
Philippe Potvin <ph.potvin@gmail.com>
2016-08-13 17:03:44 -0400

    Provide clear selection of text in terminal view (#608)
    * Permit clearSelection on text enter and mouse selection. Fix #591
    
    * Add config for copyOnSelect
    
    * Update with descriptive comment

--- app/config-default.js
+++ app/config-default.js
@@ -64,7 +64,10 @@ module.exports = {
     env: {},
 
     // set to false for no bell
-    bell: 'SOUND'
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
 
     // URL to custom bell
     // bellSoundURL: 'http://example.com/bell.mp3',
