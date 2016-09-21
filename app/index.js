bbf0af66b27010827b22dd1f770497697db0de3f
Krzysztof Kaczor <krzkaczor@gmail.com>
2016-09-22 00:11:42 +0200

    Improve developer pleasure (#556)
    * single command to run webpack and app
    
    * added dev tools and custom store for development mode only
    
    * fix linter issues

--- app/index.js
+++ app/index.js
@@ -64,7 +64,7 @@ const url = 'file://' + resolve(
 
 console.log('electron will open', url);
 
-app.on('ready', () => {
+app.on('ready', () => installDevExtensions(isDev).then(() => {
   function createWindow(fn) {
     let cfg = plugins.getDecoratedConfig();
 
@@ -375,7 +375,9 @@ app.on('ready', () => {
 
   load();
   plugins.subscribe(load);
-});
+}).catch(err => {
+  console.error('Error while loading devtools extensions', err);
+}));
 
 function initSession(opts, fn) {
   fn(uuid.v4(), new Session(opts));
@@ -394,3 +396,18 @@ app.on('open-file', (event, path) => {
     app.windowCallback = callback;
   }
 });
+
+function installDevExtensions(isDev) {
+  if (!isDev) {
+    return Promise.resolve();
+  }
+  const installer = require('electron-devtools-installer'); // eslint-disable-line global-require
+
+  const extensions = [
+    'REACT_DEVELOPER_TOOLS',
+    'REDUX_DEVTOOLS'
+  ];
+  const forceDownload = Boolean(process.env.UPGRADE_EXTENSIONS);
+
+  return Promise.all(extensions.map(name => installer.default(installer[name], forceDownload)));
+}
