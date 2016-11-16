6c1210cb7d7de716ae6f306cdb8673ffc26268cb
Matheus Fernandes <github@matheus.top>
2016-11-16 00:23:19 -0200

    `babel` => `babel-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.json/,
