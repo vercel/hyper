fbb59291e04c38bed0a06180c31c1a8a83a722c0
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-01-04 02:16:29 +0800

    Adjust namings

--- app/menu.js
+++ app/menu.js
@@ -253,7 +253,7 @@ module.exports = ({createWindow, updatePlugins}) => {
         accelerator: accelerators.maximizePane,
         click(item, focusedWindow) {
           if (focusedWindow) {
-            focusedWindow.rpc.emit('toggle pane maximized state req');
+            focusedWindow.rpc.emit('toggle pane state');
           }
         }
       }
