7ce5e6a21415f0bf90b148070991d928a389798c
jaga <kandros5591@gmail.com>
2017-01-15 18:20:43 +0100

    extract isHexFile to test/testUtils

--- test/unit/to-electron-background-color.test.js
+++ test/unit/to-electron-background-color.test.js
@@ -1,9 +1,6 @@
 import test from 'ava';
 import toElectronBackgroundColor from '../../app/utils/to-electron-background-color';
-
-function isHexColor(color) {
-  return /(^#[0-9A-F]{6,8}$)|(^#[0-9A-F]{3}$)/i.test(color); //https://regex101.com/
-}
+import {isHexColor} from '../testUtils/is-hex-color';
 
 test('toElectronBackgroundColor', t => {
   t.false(false);
@@ -13,8 +10,8 @@ test(`returns a color that's in hex`, t => {
   const hexColor = '#BADA55';
   const rgbColor = 'rgb(0,0,0)';
   const rgbaColor = 'rgb(0,0,0, 55)';
-  const hslColor = 'hsl(15, 100%, 50%)'
-  const hslaColor = 'hsl(15, 100%, 50%, 1)'
+  const hslColor = 'hsl(15, 100%, 50%)';
+  const hslaColor = 'hsl(15, 100%, 50%, 1)';
   const colorKeyword = 'pink';
 
   t.true(
