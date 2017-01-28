c5c0371fe6aa6028150fa8ff0a2d17cafcf051c1
ppot <ph.potvin@gmail.com>
2017-01-27 21:23:34 -0500

    size interpretation

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
       {
         test: /\.json/,
         loader: 'json-loader'
-      }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   // resolve: {
