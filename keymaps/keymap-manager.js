5b15455dea1e7708673f58e2c65e2a51e167823d
ppot <ph.potvin@gmail.com>
2017-02-11 19:26:19 -0500

    Add win32, linux Keymaps

--- keymaps/keymap-manager.js
+++ keymaps/keymap-manager.js
@@ -5,9 +5,17 @@ const {resolve} = require('path');
 module.exports = class KeymapManager {
   constructor() {
     this.commands = [];
-    const path = resolve('keymaps/darwin.json');
+    const path = () => {
+      switch (process.platform) {
+        case 'darwin': return resolve('keymaps/darwin.json');
+        case 'win32': return resolve('keymaps/win32.json');
+        case 'linux': return resolve('keymaps/linux.json');
+        default: return resolve('keymaps/darwin.json');
+      }
+    };
+
     try {
-      const commands = JSON.parse(readFileSync(path));
+      const commands = JSON.parse(readFileSync(path()));
       for (const command in commands) {
         if (command) {
           this.commands[command] = commands[command];
