0663ad1de4d5326eebdb5ea3257e2fce61420475
Matheus Fernandes <github@matheus.top>
2017-02-20 15:53:26 -0300

    Remove unnecessary slash

--- app/config-default.js
+++ app/config-default.js
@@ -1,6 +1,6 @@
 // Future versions of Hyper may add additional config options,
 // which will not automatically be merged into this file.
-// See https://hyper.is/#cfg for all currently supported options.
+// See https://hyper.is#cfg for all currently supported options.
 
 module.exports = {
   config: {
