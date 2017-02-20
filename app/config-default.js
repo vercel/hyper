fc829d23e8edb1160986d4eb51a9afda92403b94
Matthias Winkelmann <MatthiasWinkelmann@users.noreply.github.com>
2017-02-20 19:53:45 +0100

    Add note about future config options to default configuration (#1562)
    * add note about future config options to default configuration (fixes #1531)
    
    * Remove unnecessary slash

--- app/config-default.js
+++ app/config-default.js
@@ -1,3 +1,7 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
   config: {
     // default font size in pixels for all tabs
