ffea76b7a2663bec7333d4b4abe7a8c1d22a9ebf
ppot <ph.potvin@gmail.com>
2017-04-24 20:16:37 -0400

    comments and fnct name change

--- app/config/import.js
+++ app/config/import.js
@@ -17,13 +17,17 @@ const _write = function (path, data) {
 
 const _importConfig = function () {
   try {
+    // read config from ~/.hyper/config.js
     return readFileSync(_paths.preferencesPath, 'utf8');
   } catch (err) {
     try {
+      // read previous config on ~/.hyper.js and write to ~/.hyper/config.js
+      // write cfg to file
       const cfg = readFileSync(_paths.previousConfigPath);
       _write(_paths.preferencesPath, cfg);
       return cfg;
     } catch (err) {
+      // read default config and write cfg to file
       const cfg = readFileSync(_paths.dotConfigPath);
       _write(_paths.preferencesPath, cfg);
       return cfg;
@@ -31,14 +35,14 @@ const _importConfig = function () {
   }
 };
 
-const _importPlugins = function () {
+const _makePluginsDir = function () {
   mkdirpSync(_paths.pluginsPath);
   mkdirpSync(_paths.localPluginsPath);
 };
 
 const _import = function () {
   mkdirpSync(_paths.hyperHomeDirPath);
-  _importPlugins();
+  _makePluginsDir();
   const cfg = _init(_importConfig());
   cfg.keymaps = _keys.import(cfg.keymaps);
   return cfg;
