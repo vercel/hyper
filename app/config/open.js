c2f5361ac01170b2420b25ca5727bbd1aed50d3b
Albin Ekblom <albinekblom@gmail.com>
2017-06-09 08:51:52 +0200

    Check .js default app

--- app/config/open.js
+++ app/config/open.js
@@ -4,7 +4,26 @@ const {cfgPath} = require('./paths');
 module.exports = () => Promise.resolve(shell.openItem(cfgPath));
 
 if (process.platform === 'win32') {
-  const exec = require('child_process').exec;
+  const Registry = require('winreg');
+  const {exec} = require('child_process');
+
+  // Windows opens .js files with  WScript.exe by default
+  // If the user hasn't set up an editor for .js files, we fallback to notepad.
+  const hasDefaultApp = () => new Promise((resolve, reject) => {
+    Registry({
+      hive: Registry.HKCU,
+      key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\FileExts\\.js'
+    })
+    .keyExists((error, exists) => {
+      if (error) {
+        reject(error);
+      } else if (exists) {
+        resolve(exists);
+      } else {
+        reject(new Error('No associations for .js exists'));
+      }
+    });
+  });
 
   // This mimics shell.openItem, true if it worked, false if not.
   const openNotepad = file => new Promise(resolve => {
@@ -13,21 +32,10 @@ if (process.platform === 'win32') {
     });
   });
 
-  // Windows opens .js files with  WScript.exe by default
-  // If the user hasn't set up an editor for .js files, we fallback to notepad.
-  const canOpenNative = () => new Promise((resolve, reject) => {
-    exec('ftype JSFile', (error, stdout) => {
-      if (error) {
-        reject(error);
-      } else if (stdout && stdout.includes('WScript.exe')) {
-        reject(new Error('WScript is the default editor for .js files'));
-      } else {
-        resolve(true);
-      }
-    });
-  });
-
-  module.exports = () => canOpenNative()
+  module.exports = () => hasDefaultApp()
     .then(() => shell.openItem(cfgPath))
-    .catch(() => openNotepad(cfgPath));
+    .catch(err => {
+      console.error('Error opening config with default app:', err);
+      openNotepad(cfgPath);
+    });
 }
