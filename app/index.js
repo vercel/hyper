7fe32341dcb7f29a7b0be12447fd9ac786c05685
Albin Ekblom <albinekblom@gmail.com>
2017-06-14 05:26:09 +0200

    Cleanup escape

--- app/index.js
+++ app/index.js
@@ -264,8 +264,18 @@ app.on('ready', () => installDevExtensions(isDev).then(() => {
       session.resize({cols, rows});
     });
 
-    rpc.on('data', ({uid, data}) => {
-      sessions.get(uid).write(data);
+    rpc.on('data', ({uid, data, escaped}) => {
+      const session = sessions.get(uid);
+
+      if (escaped) {
+        const escapedData = session.shell.endsWith('cmd.exe') ?
+        `"${data}"` : // This is how cmd.exe does it
+        `'${data.replace(/'/g, `'\\''`)}'`; // Inside a single-quoted string nothing is interpreted
+
+        session.write(escapedData);
+      } else {
+        session.write(data);
+      }
     });
 
     rpc.on('open external', ({url}) => {
