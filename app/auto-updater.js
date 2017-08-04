7a017511c4ef0949fea7b3cda0dcecdb8b6f4066
Leo Lamprecht <mindrun@icloud.com>
2017-08-04 23:47:33 +0200

    New URL for update server

--- app/auto-updater.js
+++ app/auto-updater.js
@@ -4,12 +4,9 @@ const ms = require('ms');
 const notify = require('./notify'); // eslint-disable-line no-unused-vars
 const {version} = require('./package');
 
-// accepted values: `osx`, `win32`
-// https://nuts.gitbook.com/update-windows.html
-const platform = process.platform === 'darwin' ?
-  'osx' :
-  process.platform;
-const FEED_URL = `https://hyper-updates.now.sh/update/${platform}`;
+const {platform} = process;
+const FEED_URL = `https://releases.hyper.is/update/${platform}`;
+
 let isInit = false;
 
 function init() {
