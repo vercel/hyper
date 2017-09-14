b0a382a42d4625e484a06754269e2f39b50c075f
Markoz Pena Mendez <markdrew53@gmail.com>
2017-09-14 15:15:14 -0500

    Added hyperlinks to plugins

--- app/config/config-default.js
+++ app/config/config-default.js
@@ -111,7 +111,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperlinks'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
