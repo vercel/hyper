28c9884bbd40e8ac0f41f15e5718b8b615e87c0e
Wilson Tayar <wilsontayar@hotmail.com>
2017-09-18 01:04:48 -0300

    isolating the normalize function into a single module in order to stop the circular dependency issue

--- app/utils/keymaps-normalize.js
+++ app/utils/keymaps-normalize.js
@@ -1,23 +1,7 @@
-const {getKeymaps} = require('../config/keymaps');
-
-const normalize = keybinding => {
+module.exports = keybinding => {
   function sortAlphabetically(a, b) {
     return a.localeCompare(b);
   }
 
   return keybinding.toLowerCase().split('+').sort(sortAlphabetically).join('+');
-};
-
-const findCommandByKeys = (keys, commands) => {
-  return commands[normalize(keys)];
-};
-
-const getCommand = keys => {
-  return findCommandByKeys(keys, getKeymaps().keys);
-};
-
-module.exports = {
-  normalize,
-  findCommandByKeys,
-  getCommand
 };
