3a3bbae5757b1734f7f81f506027133a8c3606f5
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-09-22 17:12:26 -0700

    Add updateChannel reference

--- app/menus/menus/help.js
+++ app/menus/menus/help.js
@@ -16,10 +16,19 @@ module.exports = function (commands, showAbout) {
     {
       label: 'Report Issue',
       click() {
+        /* line 23 thru 27 is from app/menus/menu.js
+         need help figuring out how to import; the following wouldn't work:
+         const {updateChannel} = require('../menu'); */
+        const config = getConfig();
+        let updateChannel = 'stable';
+        if (config && config.updateChannel && config.updateChannel === 'canary') {
+          updateChannel = 'canary';
+        }
+
         const body = `
         <!-- Please succinctly describe your issue and steps to reproduce it. -->
         -
-        ${appName} ${appVersion}
+        ${appName} ${appVersion} (${updateChannel})
         Electron ${process.versions.electron}
         ${process.platform} ${process.arch} ${release()}
         ${JSON.stringify(getConfig(), null, 2)}
