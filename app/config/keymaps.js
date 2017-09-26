4a781be624ab4099717b8a33e20e87dfccef2dbf
CHaBou <chabup@delean.fr>
2017-09-27 00:19:05 +0200

    WIP

--- app/config/keymaps.js
+++ app/config/keymaps.js
@@ -5,13 +5,13 @@ const {defaultPlatformKeyPath} = require('./paths');
 const keys = {};
 
 const addKeys = function(keymap) {
-  Object.keys(keymap).forEach((command) => {
+  Object.keys(keymap).forEach(command => {
     if (!command) {
       return;
     }
     // We can have different keys for a same command.
     const shortcuts = Array.isArray(keymap[command]) ? keymap[command] : [keymap[command]];
-    shortcuts.forEach((shortcut) => {
+    shortcuts.forEach(shortcut => {
       keys[normalize(shortcut)] = command;
     });
   });
@@ -20,7 +20,8 @@ const addKeys = function(keymap) {
 
 const importConfig = function(customKeys) {
   try {
-    const mapping = JSON.parse(readFileSync(defaultPlatformKeyPath()));
+    const content = readFileSync(defaultPlatformKeyPath(), 'utf8');
+    const mapping = JSON.parse(content);
     addKeys(mapping);
     addKeys(customKeys);
     return keys;
