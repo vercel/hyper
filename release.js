a00be1f79cfa40b4419290442fb364c2fbfd1b81
Leo Lamprecht <mindrun@icloud.com>
2017-09-27 10:28:42 +0200

    Request release intro automatically

---
+++ release.js
@@ -0,0 +1,21 @@
+// Packages
+const {prompt} = require('inquirer');
+
+module.exports = async markdown => {
+  const answers = await prompt([{
+    name: 'intro',
+    message: 'Release intro'
+  }]);
+
+  const {intro} = answers;
+
+  if (intro === '') {
+    console.error('Please specify a release intro!');
+
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  }
+
+  markdown = `${intro}\n\n${markdown}`;
+  return markdown;
+};
