2b61d4c081777d0428739b0528003197f1564f43
Matt bc <PerStirpes@users.noreply.github.com>
2017-10-03 14:56:07 -0700

    Add config to issue report template (#2293)
    * fixes #1789

--- app/menus/menus/help.js
+++ app/menus/menus/help.js
@@ -1,5 +1,9 @@
-const os = require('os');
+const {release} = require('os');
 const {app, shell} = require('electron');
+
+const {getConfig, getPlugins} = require('../../config');
+const {arch, env, platform, versions} = process;
+const {version} = require('../../package.json');
 
 module.exports = (commands, showAbout) => {
   const submenu = [
@@ -13,11 +17,48 @@ module.exports = (commands, showAbout) => {
       label: 'Report Issue',
       click() {
         const body = `
-        <!-- Please succinctly describe your issue and steps to reproduce it. -->
-        -
-        ${app.getName()} ${app.getVersion()}
-        Electron ${process.versions.electron}
-        ${process.platform} ${process.arch} ${os.release()}`;
+<!--
+  Hi there! Thank you for discovering and submitting an issue.
+  Before you submit this; let's make sure of a few things.
+  Please make sure the following boxes are ✅ if they are correct.
+  If not, please try and fulfil these first.
+-->
+<!-- 👉 Checked checkbox should look like this: [x] -->
+  - [ ] Your Hyper.app version is **${version}**. Please verify your using the [latest](https://github.com/zeit/hyper/releases/latest) Hyper.app version
+  - [ ] I have searched the [issues](https://github.com/zeit/hyper/issues) of this repo and believe that this is not a duplicate
+
+  ---
+  - **Any relevant information from devtools?** _(CMD+ALT+I on macOS, CTRL+SHIFT+I elsewhere)_:
+<!-- 👉 Replace with info if applicable, or N/A -->
+
+  - **Is the issue reproducible in vanilla Hyper.app?**
+<!-- 👉 Replace with info if applicable, or Is Vanilla. (Vanilla means Hyper.app without any add-ons or extras. Straight out of the box.) -->
+
+## Issue
+<!-- 👉 Now feel free to write your issue, but please be descriptive! Thanks again 🙌 ❤️ -->
+
+
+
+
+
+
+<!-- ~/.hyper.js config -->
+ - **${app.getName()} version**: ${env.TERM_PROGRAM_VERSION} "${app.getVersion()}"
+
+ - **OS ARCH VERSION:** ${platform} ${arch} ${release()}
+ - **Electron:** ${versions.electron}  **LANG:** ${env.LANG}
+ - **SHELL:** ${env.SHELL}   **TERM:** ${env.TERM}
+
+  <details>
+    <summary><strong> ~/.hyper.js contents</strong></summary>
+      <pre>
+        <code>
+          ${JSON.stringify(getConfig(), null, 2)}
+
+          ${JSON.stringify(getPlugins(), null, 2)}
+        </code>
+      </pre>
+  </details>`;
 
         shell.openExternal(`https://github.com/zeit/hyper/issues/new?body=${encodeURIComponent(body)}`);
       }
@@ -35,7 +76,6 @@ module.exports = (commands, showAbout) => {
       }
     );
   }
-
   return {
     role: 'help',
     submenu
