1177cc8f905573b54ff75308820b3d3d83179fc5
Ricky Miller <ricky.miller@gmail.com>
2017-10-16 01:09:16 -0400

    Add extendedMenu

--- app/ui/contextmenu.js
+++ app/ui/contextmenu.js
@@ -4,14 +4,33 @@ const shellMenu = require('../menus/menus/shell');
 const {getKeymaps: commands} = require('../config');
 const separator = {type: 'separator'};
 
+const extendedMenu = selection => [
+  separator,
+  {
+    label: 'Open Selection as URL',
+    click() {
+      shell.openExternal(selection);
+    }
+  },
+  {
+    label: 'Search the Web for Selection',
+    click() {
+      shell.openExternal(`https://www.google.com/search?q=${selection}`);
+    }
+  }
+];
+
+// only display cut/copy when there's a cursor selection
+const filterCutCopy = (selection, menuItem) => {
+  if (/^cut$|^copy$/.test(menuItem.role) && !selection) {
+    return;
+  }
+  return menuItem;
+};
+
 module.exports = (createWindow, selection) => {
   const _shell = shellMenu(commands, createWindow).submenu;
-  const _edit = editMenu(commands).submenu.filter(menuItem => {
-    /* only display cut/copy when there's a cursor selection */
-    if (/^cut$|^copy$/.test(menuItem.role) && !selection) {
-      return
-    }
-    return menuItem
-  });
-  return _edit.concat(separator, _shell);
+  const _edit = editMenu(commands).submenu.filter(filterCutCopy.bind(null, selection));
+  const mainMenu = _edit.concat(separator, _shell);
+  return selection ? mainMenu.concat(extendedMenu(selection)) : mainMenu;
 };
