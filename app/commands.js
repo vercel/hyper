cb8b72a319789f5f74b7044b8be36cfe26471417
Renato Campos <renatoineeve@gmail.com>
2017-12-18 16:17:20 +0000

    Added keymap to rebind the 'break' command - Issue #2432 (#2531)
    * fixed documentation of roleCommands
    
    * Added keymap for break/interrupt command
    
    * added break keymap for linux and darwin
    
    * fixed lint errors

--- app/commands.js
+++ app/commands.js
@@ -93,6 +93,9 @@ const commands = {
   },
   'editor:deleteEndLine': focusedWindow => {
     focusedWindow && focusedWindow.rpc.emit('session del line end req');
+  },
+  'editor:break': focusedWindow => {
+    focusedWindow && focusedWindow.rpc.emit('session break req');
   }
 };
 
