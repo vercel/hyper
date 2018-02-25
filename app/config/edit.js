b4b308c78dbf07bb6139ab7fd290246df8147fbd
Aditya Bhansali <adityaneal@hotmail.com>
2018-02-25 11:04:21 -0600

    added error checking

---
+++ app/config/edit.js
@@ -0,0 +1,46 @@
+const {cfgPath} = require('./paths');
+const fs = require('fs');
+const recast = require('recast');
+
+function toggleUpdateChannel() {
+  // Get the config file using recast
+  const configFile = recast.parse(getFileContents(cfgPath));
+
+  // Search the config file for the property with the name 'updateChannel'
+  try {
+    // Get the config file using recast
+    const configFile = recast.parse(getFileContents(cfgPath));
+
+    // Search the config file for the property with the name 'updateChannel'
+    const updateChannel = configFile.program.body[0].expression.right.properties
+      .find(property => property.key.name === 'config')
+      .value.properties.find(property => property.key.name === 'updateChannel').value.value;
+  } catch (err) {
+      return;
+  }
+
+  // select the new value for update channel
+  updateChannel = updateChannel === 'canary' ? 'stable' : 'canary';
+  configFile.program.body[0].expression.right.properties
+    .find(property => property.key.name === 'config').value.properties
+    .find(property => property.key.name === 'updateChannel').value.value = updateChannel;
+
+  // write to the config file
+  const output = recast.print(configFile).code;
+  fs.writeFileSync(cfgPath, output, 'utf8');
+}
+
+function getFileContents(fileName) {
+  try {
+    return fs.readFileSync(fileName, 'utf8');
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+  return null;
+}
+
+module.exports = {
+  toggleUpdateChannel
+};
