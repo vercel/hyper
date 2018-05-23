e613f3e6501b801e9562df946026d80e20bb687d
Ricardo Amaral <master@ricardoamaral.net>
2018-05-23 10:26:03 +0100

    Fix linting errors

--- app/utils/wsl.js
+++ app/utils/wsl.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 // Valid Windows Subsystem for Linux shell executables
 const WSL_SHELL_EXECUTABLES = [
-	'bash.exe',
-	'wsl.exe',
-	'ubuntu.exe',
-	'ubuntu1804.exe',
-	'kali.exe',
-	'debian.exe',
-	'opensuse-42.exe',
-	'sles-12.exe'
+  'bash.exe',
+  'wsl.exe',
+  'ubuntu.exe',
+  'ubuntu1804.exe',
+  'kali.exe',
+  'debian.exe',
+  'opensuse-42.exe',
+  'sles-12.exe'
 ];
 
 // Returns true if given shell executable is from WSL, false otherwise
