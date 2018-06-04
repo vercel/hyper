vercel/hyper

commit f75895a176d421d579611ba84fdba88d1ebedab2

Fix plugin module hijacking on main process (#2866)
Fixes some plugin loading (like hyperline)

app/plugins.js
@@ -17,6 +17,8 @@ const cache = new Config();
 const path = plugs.base;
 const localPath = plugs.local;
 
+patchModuleLoad();
+
 // caches
 let plugins = config.getPlugins();
 let paths = getPaths();
@@ -42,6 +44,38 @@ config.subscribe(() => {
     }
   }
 });
+
+// patching Module._load
+// so plugins can `require` them without needing their own version
+// https://github.com/zeit/hyper/issues/619
+function patchModuleLoad() {
+  const React = require('react');
+  const PureComponent = React.PureComponent;
+  const ReactDOM = require('react-dom');
+
+  const Module = require('module');
+  const originalLoad = Module._load;
+  Module._load = function _load(modulePath) {
+    // PLEASE NOTE: Code changes here, also need to be changed in
+    // lib/utils/plugins.js
+    switch (modulePath) {
+      case 'react':
+        return React;
+      case 'react-dom':
+        return ReactDOM;
+      case 'hyper/component':
+        return PureComponent;
+      // These return Object, since they work differently on the backend, than on the frontend.
+      // Still needs to be here, to prevent errors, while loading plugins.
+      case 'hyper/Notification':
+      case 'hyper/notify':
+      case 'hyper/decorate':
+        return Object;
+      default:
+        return originalLoad.apply(this, arguments);
+    }
+  };
+}
 
 function checkDeprecatedExtendKeymaps() {
   modules.forEach(plugin => {

lib/utils/plugins.js
@@ -14,6 +14,8 @@ import notify from './notify';
 const Module = require('module');
 const originalLoad = Module._load;
 Module._load = function _load(path) {
+  // PLEASE NOTE: Code changes here, also need to be changed in
+  // app/plugins.js
   switch (path) {
     case 'react':
       return React;

commit bb185173f37178e5277da82ab8df618fa158e8bc

Fix module hijacking
Regression was introduced by #2866. Release version of Hyper failed to start.
Requiring react(-dom) in main process makes no sense.

app/plugins.js
@@ -49,24 +49,19 @@ config.subscribe(() => {
 // so plugins can `require` them without needing their own version
 // https://github.com/zeit/hyper/issues/619
 function patchModuleLoad() {
-  const React = require('react');
-  const PureComponent = React.PureComponent;
-  const ReactDOM = require('react-dom');
-
   const Module = require('module');
   const originalLoad = Module._load;
   Module._load = function _load(modulePath) {
     // PLEASE NOTE: Code changes here, also need to be changed in
     // lib/utils/plugins.js
     switch (modulePath) {
-      case 'react':
-        return React;
-      case 'react-dom':
-        return ReactDOM;
-      case 'hyper/component':
-        return PureComponent;
+      // case 'hyper/component':
+      //   return PureComponent;
       // These return Object, since they work differently on the backend, than on the frontend.
       // Still needs to be here, to prevent errors, while loading plugins.
+      // case 'react':
+      // case 'react-dom':
+      case 'hyper/component':
       case 'hyper/Notification':
       case 'hyper/notify':
       case 'hyper/decorate':