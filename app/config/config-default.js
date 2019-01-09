c4e25ed3ad41efae0ca6ab76a5fecafcfc48441c
Stan James <stan@wanderingstan.com>
2019-01-09 14:21:11 -0700

    Change directory for local plugins in comments
    https://hyper.is/#extensions-module-loading has new path for local plugins
    
    > When developing, you can add your plugin to ~/Library/Application Support/Hyper/.hyper_plugins/local and then specify it under the localPlugins array in .hyper.js.
    > ...
    > Note: plugins at ~/.hyper_plugins still supported, but will be ignored, if plugins in application directory present. Otherwise they will be moved to the application directory at first run.

--- app/config/config-default.js
+++ app/config/config-default.js
@@ -139,9 +139,12 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [],
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
+  // in development, you can create a `local` directory under
+  // `hyper_plugins` directory and include it here
   // to load it and avoid it being `npm install`ed
+  // macOS:    ~/Library/Application Support/Hyper/.hyper_plugins/local
+  // Windows:  $Env:AppData/Hyper/.hyper_plugins/local
+  // Linux:    ~/.config/Hyper/.hyper_plugins/local
   localPlugins: [],
 
   keymaps: {
