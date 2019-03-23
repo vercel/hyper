815358e4edef1f70f2efa00e508e60c455a85801
Donald Green <donald.green@medlmobile.com>
2019-03-22 23:41:59 -0700

    Adjustments and regression issues fixed
    1. Updated the default config file to better explain the supported options for the "bell" config property.
    2. Rearranged the bellSoundURL default property to make it easier to find should one decide to change the bell sound.
    3. Typos fixed in comments.
    4. Update fetchFileData to utilize the configData provided as function argument. There appeared to be no reason to reference different sources of config data within the same method.

--- app/config/config-default.js
+++ app/config/config-default.js
@@ -112,8 +112,13 @@ module.exports = {
     // for environment variables
     env: {},
 
-    // set to `false` for no bell
+    // Supported Options:
+    //  1. 'SOUND' -> Enables the bell as a sound
+    //  2. false: turns off the bell
     bell: 'SOUND',
+
+    // An absolute file path to a sound file on the machine.
+    // bellSoundURL: '/path/to/sound/file',
 
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: false,
@@ -129,9 +134,6 @@ module.exports = {
     // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
     // (inside tmux or vim with mouse mode enabled for example).
     macOptionSelectionMode: 'vertical',
-
-    // Either an absolute file path to a sound file on the machine.
-    // bellSoundURL: '/path/to/sound/file',
 
     // Whether to use the WebGL renderer. Set it to false to use canvas-based
     // rendering (slower, but supports transparent backgrounds)
