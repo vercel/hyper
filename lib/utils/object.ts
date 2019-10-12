caac124575646c7670caef664b51a607af41aa4a
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2019-10-12 15:46:45 +0530

    Fix ts errors in lib/utils

--- lib/utils/object.ts
+++ lib/utils/object.ts
@@ -1,6 +1,6 @@
 const valsCache = new WeakMap();
 
-export function values(imm) {
+export function values(imm: Record<string, any>) {
   if (!valsCache.has(imm)) {
     valsCache.set(imm, Object.values(imm));
   }
@@ -8,7 +8,7 @@ export function values(imm) {
 }
 
 const keysCache = new WeakMap();
-export function keys(imm) {
+export function keys(imm: Record<string, any>) {
   if (!keysCache.has(imm)) {
     keysCache.set(imm, Object.keys(imm));
   }
--- lib/utils/term-groups.ts
+++ lib/utils/term-groups.ts
@@ -1,4 +1,7 @@
-export default function findBySession(termGroupState, sessionUid) {
+import {ITermState} from '../hyper';
+import {Immutable} from 'seamless-immutable';
+
+export default function findBySession(termGroupState: Immutable<ITermState>, sessionUid: string) {
   const {termGroups} = termGroupState;
   return Object.keys(termGroups)
     .map(uid => termGroups[uid])
