aa0fc9ac53dd9dc8db2d908d80513233e3f35122
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2019-11-20 18:57:56 +0530

    Fix lint errors

--- lib/selectors.ts
+++ lib/selectors.ts
@@ -2,12 +2,10 @@ import {createSelector} from 'reselect';
 import {HyperState} from './hyper';
 
 const getTermGroups = ({termGroups}: Pick<HyperState, 'termGroups'>) => termGroups.termGroups;
-const getRootGroups = createSelector(
-  getTermGroups,
-  termGroups =>
-    Object.keys(termGroups)
-      .map(uid => termGroups[uid])
-      .filter(({parentUid}) => !parentUid)
+const getRootGroups = createSelector(getTermGroups, termGroups =>
+  Object.keys(termGroups)
+    .map(uid => termGroups[uid])
+    .filter(({parentUid}) => !parentUid)
 );
 
 export default getRootGroups;
