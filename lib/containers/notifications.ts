c337288efa74acec63f4ba37f1abbd2f469f068b
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2020-02-19 21:11:29 +0530

    Improve containers typings and add prop types

--- lib/containers/notifications.ts
+++ lib/containers/notifications.ts
@@ -2,75 +2,96 @@ import Notifications from '../components/notifications';
 import {installUpdate} from '../actions/updater';
 import {connect} from '../utils/plugins';
 import {dismissNotification} from '../actions/notifications';
-import {HyperState} from '../hyper';
-import {Dispatch} from 'redux';
+import {HyperState, HyperDispatch} from '../hyper';
 
-const NotificationsContainer = connect(
-  (state: HyperState) => {
-    const {ui} = state;
-    const {notifications} = ui;
-    const state_ = {};
+const mapStateToProps = (state: HyperState) => {
+  const {ui} = state;
+  const {notifications} = ui;
+  let state_: Partial<{
+    fontShowing: boolean;
+    fontSize: number;
+    fontText: string;
+    resizeShowing: boolean;
+    cols: number | null;
+    rows: number | null;
+    updateShowing: boolean;
+    updateVersion: string | null;
+    updateNote: string | null;
+    updateReleaseUrl: string | null;
+    updateCanInstall: boolean | null;
+    messageShowing: boolean;
+    messageText: string | null;
+    messageURL: string | null;
+    messageDismissable: boolean | null;
+  }> = {};
 
-    if (notifications.font) {
-      const fontSize = ui.fontSizeOverride || ui.fontSize;
+  if (notifications.font) {
+    const fontSize = ui.fontSizeOverride || ui.fontSize;
 
-      Object.assign(state_, {
-        fontShowing: true,
-        fontSize,
-        fontText: `${fontSize}px`
-      });
-    }
-
-    if (notifications.resize) {
-      const cols = ui.cols;
-      const rows = ui.rows;
-
-      Object.assign(state_, {
-        resizeShowing: true,
-        cols,
-        rows
-      });
-    }
-
-    if (notifications.updates) {
-      Object.assign(state_, {
-        updateShowing: true,
-        updateVersion: ui.updateVersion,
-        updateNote: ui.updateNotes!.split('\n')[0],
-        updateReleaseUrl: ui.updateReleaseUrl,
-        updateCanInstall: ui.updateCanInstall
-      });
-    } else if (notifications.message) {
-      Object.assign(state_, {
-        messageShowing: true,
-        messageText: ui.messageText,
-        messageURL: ui.messageURL,
-        messageDismissable: ui.messageDismissable
-      });
-    }
-
-    return state_;
-  },
-  (dispatch: Dispatch<any>) => {
-    return {
-      onDismissFont: () => {
-        dispatch(dismissNotification('font'));
-      },
-      onDismissResize: () => {
-        dispatch(dismissNotification('resize'));
-      },
-      onDismissUpdate: () => {
-        dispatch(dismissNotification('updates'));
-      },
-      onDismissMessage: () => {
-        dispatch(dismissNotification('message'));
-      },
-      onUpdateInstall: () => {
-        dispatch(installUpdate());
-      }
+    state_ = {
+      ...state_,
+      fontShowing: true,
+      fontSize,
+      fontText: `${fontSize}px`
     };
-  },
-  null
-)(Notifications, 'Notifications');
+  }
+
+  if (notifications.resize) {
+    const cols = ui.cols;
+    const rows = ui.rows;
+
+    state_ = {
+      ...state_,
+      resizeShowing: true,
+      cols,
+      rows
+    };
+  }
+
+  if (notifications.updates) {
+    state_ = {
+      ...state_,
+      updateShowing: true,
+      updateVersion: ui.updateVersion,
+      updateNote: ui.updateNotes!.split('\n')[0],
+      updateReleaseUrl: ui.updateReleaseUrl,
+      updateCanInstall: ui.updateCanInstall
+    };
+  } else if (notifications.message) {
+    state_ = {
+      ...state_,
+      messageShowing: true,
+      messageText: ui.messageText,
+      messageURL: ui.messageURL,
+      messageDismissable: ui.messageDismissable
+    };
+  }
+
+  return state_;
+};
+
+const mapDispatchToProps = (dispatch: HyperDispatch) => {
+  return {
+    onDismissFont: () => {
+      dispatch(dismissNotification('font'));
+    },
+    onDismissResize: () => {
+      dispatch(dismissNotification('resize'));
+    },
+    onDismissUpdate: () => {
+      dispatch(dismissNotification('updates'));
+    },
+    onDismissMessage: () => {
+      dispatch(dismissNotification('message'));
+    },
+    onUpdateInstall: () => {
+      dispatch(installUpdate());
+    }
+  };
+};
+
+const NotificationsContainer = connect(mapStateToProps, mapDispatchToProps, null)(Notifications, 'Notifications');
 
 export default NotificationsContainer;
+
+export type NotificationsConnectedProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
--- lib/containers/terms.ts
+++ lib/containers/terms.ts
@@ -4,80 +4,80 @@ import {resizeSession, sendSessionData, setSessionXtermTitle, setActiveSession, 
 
 import {openContextMenu} from '../actions/ui';
 import {getRootGroups} from '../selectors';
-import {HyperState, TermsProps} from '../hyper';
-import {Dispatch} from 'redux';
+import {HyperState, HyperDispatch} from '../hyper';
 
-const TermsContainer = connect(
-  (state: HyperState): TermsProps => {
-    const {sessions} = state.sessions;
-    return {
-      sessions,
-      cols: state.ui.cols,
-      rows: state.ui.rows,
-      scrollback: state.ui.scrollback,
-      termGroups: getRootGroups(state),
-      activeRootGroup: state.termGroups.activeRootGroup,
-      activeSession: state.sessions.activeUid,
-      customCSS: state.ui.termCSS,
-      write: state.sessions.write,
-      fontSize: state.ui.fontSizeOverride ? state.ui.fontSizeOverride : state.ui.fontSize,
-      fontFamily: state.ui.fontFamily,
-      fontWeight: state.ui.fontWeight,
-      fontWeightBold: state.ui.fontWeightBold,
-      lineHeight: state.ui.lineHeight,
-      letterSpacing: state.ui.letterSpacing,
-      uiFontFamily: state.ui.uiFontFamily,
-      fontSmoothing: state.ui.fontSmoothingOverride,
-      padding: state.ui.padding,
-      cursorColor: state.ui.cursorColor,
-      cursorAccentColor: state.ui.cursorAccentColor,
-      cursorShape: state.ui.cursorShape,
-      cursorBlink: state.ui.cursorBlink,
-      borderColor: state.ui.borderColor,
-      selectionColor: state.ui.selectionColor,
-      colors: state.ui.colors,
-      foregroundColor: state.ui.foregroundColor,
-      backgroundColor: state.ui.backgroundColor,
-      bell: state.ui.bell,
-      bellSoundURL: state.ui.bellSoundURL,
-      bellSound: state.ui.bellSound,
-      copyOnSelect: state.ui.copyOnSelect,
-      modifierKeys: state.ui.modifierKeys,
-      quickEdit: state.ui.quickEdit,
-      webGLRenderer: state.ui.webGLRenderer,
-      macOptionSelectionMode: state.ui.macOptionSelectionMode,
-      disableLigatures: state.ui.disableLigatures
-    };
-  },
-  (dispatch: Dispatch<any>) => {
-    return {
-      onData(uid: string, data: any) {
-        dispatch(sendSessionData(uid, data));
-      },
+const mapStateToProps = (state: HyperState) => {
+  const {sessions} = state.sessions;
+  return {
+    sessions,
+    cols: state.ui.cols,
+    rows: state.ui.rows,
+    scrollback: state.ui.scrollback,
+    termGroups: getRootGroups(state),
+    activeRootGroup: state.termGroups.activeRootGroup,
+    activeSession: state.sessions.activeUid,
+    customCSS: state.ui.termCSS,
+    write: state.sessions.write,
+    fontSize: state.ui.fontSizeOverride ? state.ui.fontSizeOverride : state.ui.fontSize,
+    fontFamily: state.ui.fontFamily,
+    fontWeight: state.ui.fontWeight,
+    fontWeightBold: state.ui.fontWeightBold,
+    lineHeight: state.ui.lineHeight,
+    letterSpacing: state.ui.letterSpacing,
+    uiFontFamily: state.ui.uiFontFamily,
+    fontSmoothing: state.ui.fontSmoothingOverride,
+    padding: state.ui.padding,
+    cursorColor: state.ui.cursorColor,
+    cursorAccentColor: state.ui.cursorAccentColor,
+    cursorShape: state.ui.cursorShape,
+    cursorBlink: state.ui.cursorBlink,
+    borderColor: state.ui.borderColor,
+    selectionColor: state.ui.selectionColor,
+    colors: state.ui.colors,
+    foregroundColor: state.ui.foregroundColor,
+    backgroundColor: state.ui.backgroundColor,
+    bell: state.ui.bell,
+    bellSoundURL: state.ui.bellSoundURL,
+    bellSound: state.ui.bellSound,
+    copyOnSelect: state.ui.copyOnSelect,
+    modifierKeys: state.ui.modifierKeys,
+    quickEdit: state.ui.quickEdit,
+    webGLRenderer: state.ui.webGLRenderer,
+    macOptionSelectionMode: state.ui.macOptionSelectionMode,
+    disableLigatures: state.ui.disableLigatures
+  };
+};
 
-      onTitle(uid: string, title: string) {
-        dispatch(setSessionXtermTitle(uid, title));
-      },
+const mapDispatchToProps = (dispatch: HyperDispatch) => {
+  return {
+    onData(uid: string, data: any) {
+      dispatch(sendSessionData(uid, data));
+    },
 
-      onResize(uid: string, cols: number, rows: number) {
-        dispatch(resizeSession(uid, cols, rows));
-      },
+    onTitle(uid: string, title: string) {
+      dispatch(setSessionXtermTitle(uid, title));
+    },
 
-      onActive(uid: string) {
-        dispatch(setActiveSession(uid));
-      },
-      toggleSearch(uid: string) {
-        dispatch(onSearch(uid));
-      },
+    onResize(uid: string, cols: number, rows: number) {
+      dispatch(resizeSession(uid, cols, rows));
+    },
 
-      onContextMenu(uid: string, selection: any) {
-        dispatch(setActiveSession(uid));
-        dispatch(openContextMenu(uid, selection));
-      }
-    };
-  },
-  null,
-  {forwardRef: true}
-)(Terms, 'Terms');
+    onActive(uid: string) {
+      dispatch(setActiveSession(uid));
+    },
+    toggleSearch(uid: string) {
+      dispatch(onSearch(uid));
+    },
+
+    onContextMenu(uid: string, selection: any) {
+      dispatch(setActiveSession(uid));
+      dispatch(openContextMenu(uid, selection));
+    }
+  };
+};
+
+const TermsContainer = connect(mapStateToProps, mapDispatchToProps, null, {forwardRef: true})(Terms, 'Terms');
 
 export default TermsContainer;
+
+export type TermsConnectedProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
