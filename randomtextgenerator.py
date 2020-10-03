ce0194bf51d56a408c2eec5d7192e193d6e026fd
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-10-03 18:35:57 +0530

    Create randomtextgenerator.py

---
+++ randomtextgenerator.py
@@ -0,0 +1,13 @@
+import string 
+import random 
+  
+# initializing size of string  
+N = 7
+  
+# using random.choices() 
+# generating random strings  
+res = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k = N)) 
+  
+# print result 
+print("The generated random string : " + str(res)) 
