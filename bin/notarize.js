3c5a4c74570687c0189119c371b559f466e22b87
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-10-03 12:36:59 +0530

    Support notarization on macOS

---
+++ bin/notarize.js
@@ -0,0 +1,16 @@
+const { notarize } = require("electron-notarize");
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin" || !process.env.APPLE_ID || !process.env.APPLE_PASSWORD) {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  return await notarize({
+    appBundleId: "co.zeit.hyper",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_PASSWORD
+  });
+};
