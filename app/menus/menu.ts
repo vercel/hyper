160dd1220d2e5e7344d7b592bfa50a5bbf84476d
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-10-11 21:03:35 +0530

    Bump copyright year to 2021

--- app/menus/menu.ts
+++ app/menus/menu.ts
@@ -54,7 +54,7 @@ export const createMenu = (
     void dialog.showMessageBox({
       title: `About ${appName}`,
       message: `${appName} ${appVersion} (${updateChannel})`,
-      detail: `Renderers: ${renderers}\nPlugins: ${pluginList}\n\nCreated by Guillermo Rauch\nCopyright © 2020 Vercel, Inc.`,
+      detail: `Renderers: ${renderers}\nPlugins: ${pluginList}\n\nCreated by Guillermo Rauch\nCopyright © 2021 Vercel, Inc.`,
       buttons: [],
       icon: icon as any
     });
