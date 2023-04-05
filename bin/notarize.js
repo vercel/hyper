2bbb8775f6409b7594d0095803f0435c27860203
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2023-04-05 04:33:47 +0000

    update electron and electron-builder

--- bin/notarize.js
+++ bin/notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require("electron-notarize");
+const { notarize } = require("@electron/notarize");
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
