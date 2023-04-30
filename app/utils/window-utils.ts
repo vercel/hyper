b353a282adaedaa0effaa46070a981d81c4cfab3
Haneen Mahdin <haneenmahdin@gmail.com>
2023-04-30 16:33:28 +0530

    perf: filter displays based on the intersection with the target position
    I have updated the `positionIsValid()` function to filter out displays the only have intersection with the target point. This improves performance.

--- app/utils/window-utils.ts
+++ app/utils/window-utils.ts
@@ -1,10 +1,19 @@
 import electron from 'electron';
 
+/**
+ * Check if target position falls within workArea of each filtered display
+ */
 export function positionIsValid(position: [number, number]) {
   const displays = electron.screen.getAllDisplays();
   const [x, y] = position;
 
-  return displays.some(({workArea}) => {
+  // Filter displays based on intersection with target position
+  const filteredDisplays = displays.filter(({bounds}) => {
+    return x >= bounds.x && x <= bounds.x + bounds.width && y >= bounds.y && y <= bounds.y + bounds.height;
+  });
+
+  // Run the check
+  return filteredDisplays.some(({workArea}) => {
     return x >= workArea.x && x <= workArea.x + workArea.width && y >= workArea.y && y <= workArea.y + workArea.height;
   });
 }
