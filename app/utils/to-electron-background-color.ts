39c0a5673ab4d40620ff0664fa7f13387fd9d690
Haneen Mahdin <haneenmahdin@gmail.com>
2023-04-30 16:43:17 +0530

    perf: cache background colors converted to support electron
    The utility electron background color converts colors for electron support. I have added a new object which is optimised for frequent reads and updates. This saves processing time and power.

--- app/utils/to-electron-background-color.ts
+++ app/utils/to-electron-background-color.ts
@@ -1,14 +1,21 @@
 // Packages
 import Color from 'color';
 
+const colorCache: Record<string, Color> = {};
+
 // returns a background color that's in hex
 // format including the alpha channel (e.g.: `#00000050`)
 // input can be any css value (rgb, hsl, string…)
 export default (bgColor: string) => {
-  const color = Color(bgColor);
+  let color = colorCache[bgColor];
+  if (!color) {
+    color = Color(bgColor);
+    colorCache[bgColor] = color;
+  }
 
-  if (color.alpha() === 1) {
-    return color.hex().toString();
+  const alpha = color.alpha();
+  if (alpha === 1) {
+    return color.hex();
   }
 
   // http://stackoverflow.com/a/11019879/1202488
