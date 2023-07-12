51276567637532dfbe5d7cabcdeebe3020b3b9ad
bruno henrique <bh.bsb2011@hotmail.com>
2023-07-11 22:00:43 -0300

    remove comments

--- app/utils/session-has-children.ts
+++ app/utils/session-has-children.ts
@@ -13,16 +13,3 @@ export default function sessionHasRunningChildren(session: Session): boolean {
     return false;
   }
 }
-
-// export default function sessionHasRunningChildren(session: Session): boolean {
-//   if (process.platform == 'linux') {
-//     if (session.pty != null) {
-//       const childProcInfoPath = `/proc/${session.pty.pid}/task/${session.pty.pid}/children`;
-//       return readFileSync(childProcInfoPath, 'utf8').length >= 1;
-//     } else {
-//       return false;
-//     }
-//   } else {
-//     return false;
-//   }
-// }
